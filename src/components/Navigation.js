65cbf035c7c26d5ff766c505da965fb54a83f2bb
Jonathan Scott <jonathan.scott@gmail.com>
2019-07-22 12:36:11 -0700

    finished nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { Contact, Home, About } from '.';
 
 const Navigation = () => {
   return (
-    <div>
-      <div className="App">
-        <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
+      <div>
+        <div className="App">
+          <h1>React Router Mini</h1>
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/about/">About</Link>
+          </div>
+          <div>
+            <Link to="/contact/">Contact</Link>
+          </div>
         </div>
       </div>
-    </div>
+
   );
 };
 
